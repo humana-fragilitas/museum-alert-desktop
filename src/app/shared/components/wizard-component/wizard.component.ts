85243d6c6aa96bea20c17f3c889904479ba230d9
Andrea Blasio <andrea@humanafragilitas.com>
2025-03-14 15:57:36 +0100

    Minor refactoring of wizard component

--- src/app/shared/components/wizard-component/wizard.component.ts
+++ src/app/shared/components/wizard-component/wizard.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation, AfterViewInit } from '@angular/core';
 import { WiFiCredentialsComponent } from '../wifi-credentials/wifi-credentials.component';
 import { ProvisioningService } from '../../../core/services/provisioning/provisioning.service';
 import { DeviceService } from '../../../core/services/device/device.service';
-import { combineLatest, Subscription, takeUntil } from 'rxjs';
+import { combineLatest, Subject, takeUntil } from 'rxjs';
 import { DeviceAppState } from '@shared/models';
 import { MatStepper } from '@angular/material/stepper';
 import { AuthService } from '../../../core/services/auth/auth.service';
@@ -11,74 +11,78 @@ import { AuthService } from '../../../core/services/auth/auth.service';
   selector: 'app-wizard',
   templateUrl: './wizard.component.html',
   styleUrls: ['./wizard.component.scss'],
-  imports: [ WiFiCredentialsComponent ],
+  imports: [WiFiCredentialsComponent],
   encapsulation: ViewEncapsulation.None
 })
-export class WizardComponent implements OnInit, OnDestroy {
+export class WizardComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  private deviceCombinedStateSubscription: Subscription;
+  private destroy$ = new Subject<void>();
 
   isVisible: boolean = false;
   isReady: boolean = false;
+
   @ViewChild('stepper') stepper!: MatStepper;
+
+  private latestAppStatus: Nullable<DeviceAppState> = null;
 
   constructor(
     public deviceService: DeviceService,
     private provisioningService: ProvisioningService,
     private authService: AuthService
-  ) {
-
-    this.deviceCombinedStateSubscription = combineLatest(
-      [this.deviceService.connectionStatus$, this.deviceService.deviceAppStatus$]
-    ).subscribe(([isConnected, appStatus]) => {
-
-      this.setStepperState(appStatus);
-      this.isVisible = isConnected;
-      this.isReady = isConnected && appStatus != DeviceAppState.STARTED;
-
-    });
-
-  }
+  ) {}
 
   ngOnInit(): void {
     console.log('WizardComponent INIT');
 
+    combineLatest([
+      this.deviceService.connectionStatus$,
+      this.deviceService.deviceAppStatus$
+    ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(([isConnected, appStatus]) => {
+        this.isVisible = isConnected;
+        this.isReady = isConnected && appStatus !== DeviceAppState.STARTED;
+
+        // Store the latest app state for use in ngAfterViewInit
+        this.latestAppStatus = appStatus;
+
+        // If ViewChild is already available, update stepper
+        if (this.stepper) {
+          this.setStepperState(appStatus);
+        }
+      });
+  }
+
+  ngAfterViewInit(): void {
+    // Once the view is ready, update the stepper with the latest app status
+    if (this.latestAppStatus) {
+      this.setStepperState(this.latestAppStatus);
+    }
   }
 
   ngOnDestroy(): void {
-
-    this.deviceCombinedStateSubscription.unsubscribe();
-
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   createProvisioningClaim() {
-
-    this.provisioningService.createClaim().subscribe((claim:any)=> {
-
+    this.provisioningService.createClaim().subscribe((claim: any) => {
       const testBluetoothPayload = {
         tempCertPem: claim.certificatePem,
         tempPrivateKey: claim.keyPair.PrivateKey
       };
+
       console.log("<|" + JSON.stringify(testBluetoothPayload) + "|>");
       console.log(claim);
 
-      const idToken = this.authService
-                          .sessionData
-                          .value
-                          ?.tokens
-                          ?.idToken
-                          ?.toString();
+      const idToken = this.authService.sessionData.value?.tokens?.idToken?.toString();
 
       this.deviceService.sendData({ ...testBluetoothPayload, idToken });
-      
     });
-
   }
 
   setStepperState(state: Nullable<DeviceAppState>) {
-
     switch (state) {
-
       case DeviceAppState.CONFIGURE_WIFI:
         this.stepper.steps.get(0)!.editable = true;
         this.stepper.selectedIndex = 0;
@@ -97,11 +101,6 @@ export class WizardComponent implements OnInit, OnDestroy {
         this.stepper.selected!.completed = true;
         this.stepper.selectedIndex = 3;
         break;
-
     }
-
-  } 
-
-
-
+  }
 }
