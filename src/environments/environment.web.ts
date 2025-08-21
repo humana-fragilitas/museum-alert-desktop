8ca5cda61e174f027fc14bc942e6daa8902f865a
Andrea Blasio <andrea@humanafragilitas.com>
2025-08-21 19:23:23 +0200

    Updates configuration

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,9 +1,9 @@
-// Copy and paste in src/app/environments/environment.*.ts
+        // Copy and paste in src/app/environments/environment.*.ts
         export const APP_CONFIG = {
           production: false,
           environment: 'DEV',
           aws: {
-            apiGateway: 'https://30pyi8odpi.execute-api.eu-west-2.amazonaws.com/dev',
+            apiGateway: 'https://157orq78ak.execute-api.eu-west-2.amazonaws.com/dev',
             region: 'eu-west-2',
             algorithm: 'AWS4-HMAC-SHA256',
             IoTCore: {
@@ -13,9 +13,9 @@
             amplify: {
               Auth: {
                 Cognito: {
-                  userPoolId: 'eu-west-2_rHTWtQJv4',
-                  userPoolClientId: '6fnob0mojsco76gmt4gnd4ultr',
-                  identityPoolId: 'eu-west-2:82cdd6eb-f806-48dd-9d71-8e0c1982f32a',
+                  userPoolId: 'eu-west-2_ip8Ic8yEj',
+                  userPoolClientId: '4fg3uneoltc3e3ecj96qeuchhc',
+                  identityPoolId: 'eu-west-2:792590fa-8cbf-4eab-af4c-8d0f6139e603',
                   mandatorySignIn: true,
                   authenticationFlowType: 'USER_SRP_AUTH'
                 }
